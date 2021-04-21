66806c1561426e68bd7e343ad325563184960ad8
Irina.Petrovskaya <irina.petrovskaya@jetbrains.com>
2021-04-21 14:28:35 +0300

    Initial commit

---
+++ src/main/java/com/mytests/ip/myspringinitializr/MySpringInitializrApplication.java
@@ -0,0 +1,13 @@
+package com.mytests.ip.myspringinitializr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringInitializrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MySpringInitializrApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mytests/ip/myspringinitializr/MySpringInitializrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mytests.ip.myspringinitializr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySpringInitializrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
